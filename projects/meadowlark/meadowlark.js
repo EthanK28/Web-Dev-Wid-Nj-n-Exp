6ffacb8c9f6ea50dc342742e9065f34bf58ccc19
EthanK28 <kes0228@gmail.com>
2015-10-08 18:39:25 +0900

    form processing by ajax, or normally

--- projects/meadowlark/meadowlark.js
+++ projects/meadowlark/meadowlark.js
@@ -6,9 +6,21 @@ var app = express();
 
 app.use(express.static(__dirname+'/public'));
 
+app.use(require('body-parser')());
+
+
 // Set up handlebars view engine
 var handlebars = require('express-handlebars')
-                    .create({ defaultLayout: 'main' });
+                    .create({
+                      defaultLayout: 'main',
+                      helpers: {
+                          section: function(name, options) {
+                            if(!this._sections) this._sections = {};
+                            this._sections[name] = options.fn(this);
+                            return null;
+                          }
+                        }
+                     });
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -41,6 +53,41 @@ app.get('/about', function(req, res){
 });
 
 
+app.get('/newsletter', function(req, res){
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+
+app.get('/thank-you', function(req, res) {
+    res.render('thank-you');
+});
+
+app.get('/newsletterajax', function(req, res){
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+    res.render('newsletterajax', { csrf: 'CSRF token goes here' });
+});
+
+
+app.post('/process', function(req, res){
+  console.log('Form (from querystring): ' + req.query.form);
+  console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+  console.log('Name (from visible form field): ' + req.body.name);
+  console.log('Email (from visible form field): ' + req.body.email);
+  res.redirect(303, '/thank-you');
+});
+
+app.post('/process2', function(req, res){
+if(req.xhr || req.accepts('json,html')==='json'){
+        // if there were an error, we would send { error: 'error description' }
+res.send({ success: true }); } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, '/thank-you');
+    }
+});
+
+
 app.get('/tours/hood-river', function(req, res){
 	res.render('tours/hood-river');
 });
@@ -69,7 +116,16 @@ app.get('/greeting', function(req, res){ res.render('about', {
                 style: req.query.style,
                 userid: req.cookie.userid,
                 username: req.session.username,
-}); });
+              });
+});
+
+var tours = [
+  { id: 0, name: 'Hood River', price: 99.99 },
+  { id: 1, name: 'Oregon Coast', price: 149.95 },
+];
+
+
+
 
 
 // custom 404 page
